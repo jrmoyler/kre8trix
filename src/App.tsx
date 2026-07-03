faa84e57216060d321baf0a7bf723077e88695bc
jrmoyler <jrmoyler93@gmail.com>
2026-07-02 20:26:59 -0400

    Add App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import { Routes, Route, Navigate } from 'react-router'
+import Layout from './components/Layout'
+import Dashboard from './pages/Dashboard'
+import Wallet from './pages/Wallet'
+import CreditScore from './pages/CreditScore'
+import CashFlow from './pages/CashFlow'
+import Advances from './pages/Advances'
+import CardManager from './pages/CardManager'
+import Analytics from './pages/Analytics'
+import Onboarding from './pages/Onboarding'
+import Settings from './pages/Settings'
+
+export default function App() {
+  return (
+    <Layout>
+      <Routes>
+        <Route path="/" element={<Navigate to="/dashboard" replace />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/wallet" element={<Wallet />} />
+        <Route path="/credit-score" element={<CreditScore />} />
+        <Route path="/cash-flow" element={<CashFlow />} />
+        <Route path="/advances" element={<Advances />} />
+        <Route path="/cards" element={<CardManager />} />
+        <Route path="/analytics" element={<Analytics />} />
+        <Route path="/onboarding" element={<Onboarding />} />
+        <Route path="/settings" element={<Settings />} />
+      </Routes>
+    </Layout>
+  )
+}
